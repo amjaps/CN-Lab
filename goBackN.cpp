b224d194312dd1511dbe84469382eb027740f8a7
Amar Soni <amar.amjaps.verma@gmail.com>
2021-01-12 11:43:46 +0530

    goBackN

---
+++ goBackN.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, i, retransmission;
+    cout << "Enter the number of packets to be transmitted: ";
+    cin >> n;
+    cout << "Enter the value of i: ";
+    cin >> i;
+    
+    return 0;
+}
